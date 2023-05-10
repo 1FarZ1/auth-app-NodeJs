2a58d69ac4372d2ade9da33b602f0114eb6f4e67
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-10 10:28:21 +0100

    structuring the app done

--- routes/tasks.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllTasks, getTask, updateTasks, deleteTask, createTask } = require('../controllers/tasks.js');
-const Tasksrouter = express.Router();
-
-Tasksrouter.route('/').get(getAllTasks).post(createTask)
-Tasksrouter.route('/:id').get(getTask).patch(updateTasks).delete(deleteTask)
-
-
-module.exports = Tasksrouter;
