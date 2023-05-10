2eaa8e2fff3ba3a1743015335046016ff673e7db
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2023-05-10 10:24:34 +0100

    1st struct of the project

---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const Tasksrouter = require('./routes/tasks');
+const {  connectDb } = require('./db/mongoDb');
+const notFound = require('./middlewares/not_found');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static('./views'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+// app.use("/api/v1/tasks",Tasksrouter);
+
+app.use(notFound);  
+
+
+
+
+
+
+let main = async ()=>{  
+try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, () => {
+    console.log('Server is running on http://localhost:' + port);
+    }
+);
+} catch (error) {
+    console.log(error);    
+}
+}
+
+main();
+
+
